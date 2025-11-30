2410ac6a3fd08ad268df4d168c62fbcd675f5d04
jan-riemenschneider <jan-riemenschneider@outlook.de>
2025-11-30 15:31:31 +0100

    remove nativwind 5.0 and install 4.0 for better stability

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,11 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Text } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { Collapsible } from '@/components/ui/collapsible';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
 
@@ -30,7 +30,10 @@ export default function TabTwoScreen() {
           Explore
         </ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
+
+      <Text className="flex-1 font-bold">
+        This app includesff example code to help you get started.
+      </Text>
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
